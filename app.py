11f8d002d79c2411e8d69e146953bd68edc441f6
John Curanaj <Jcuranaj24@gmail.com>
2023-03-31 14:09:55 -0400

    added repairToolbox router

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from auth.jwt_bearer import JWTBearer
 from config.config import initiate_database
 from routes.admin import router as AdminRouter
 from routes.substratePowder import router as SubstratePowderRouter
+from routes.repairToolbox import router as repairToolboxRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -36,3 +37,5 @@ async def read_root():
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
 app.include_router(SubstratePowderRouter, tags=["SubstratePowder"], prefix="/substratePowder")
+app.include_router(repairToolboxRouter, tags=["RepairToolbox"], prefix="/repairToolbox")
+
